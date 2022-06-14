bcba3f589b934ab32672e3e9ac9671d6f726d4d8
LuYomayel <luciano.yomayel@alumnos.frgp.utn.edu.ar>
2022-06-14 09:06:42 -0300

    WebServer

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express')
+const hbs = require('hbs');
+const app = express();
+const port = process.env.PORT;
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estatico
+app.use( express.static('public'))
+
+app.get('/', (req,res)=> {
+    res.render('home', {
+        nombre: 'Luciano',
+        titulo: 'Alta pagina'
+    })
+});
+
+app.get('/generic', (req,res)=> {
+    res.render('generic', {
+        nombre: 'Luciano',
+        titulo: 'Alta pagina'
+    })
+});
+
+app.get('/elements', (req,res)=> {
+    res.render('elements', {
+        nombre: 'Luciano',
+        titulo: 'Alta pagina'
+    })
+});
+
+app.listen(port, () => console.log(`Listening to ${port}`))
+
+app.get('*', (req, res) => {
+    res.send('404 | Page not found')
+})
