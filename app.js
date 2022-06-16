a6b598cd33347438cc59393b623462a533d0ed74
LuYomayel <luciano.yomayel@alumnos.frgp.utn.edu.ar>
2022-06-15 22:38:27 -0300

    frontend

--- app.js
+++ app.js
@@ -10,25 +10,29 @@ hbs.registerPartials(__dirname + '/views/partials');
 // Servir contenido estatico
 app.use( express.static('public'))
 
-app.get('/', (req,res)=> {
-    res.render('home', {
-        nombre: 'Luciano',
-        titulo: 'Alta pagina'
-    })
-});
+// app.get('/', (req,res)=> {
+//     res.render('home', {
+//         nombre: 'Luciano',
+//         titulo: 'Alta pagina'
+//     })
+// });
 
-app.get('/generic', (req,res)=> {
-    res.render('generic', {
-        nombre: 'Luciano',
-        titulo: 'Alta pagina'
-    })
-});
+// app.get('/generic', (req,res)=> {
+//     res.render('generic', {
+//         nombre: 'Luciano',
+//         titulo: 'Alta pagina'
+//     })
+// });
 
-app.get('/elements', (req,res)=> {
-    res.render('elements', {
-        nombre: 'Luciano',
-        titulo: 'Alta pagina'
-    })
+// app.get('/elements', (req,res)=> {
+//     res.render('elements', {
+//         nombre: 'Luciano',
+//         titulo: 'Alta pagina'
+//     })
+// });
+
+app.get('*', (req,res)=> {
+    res.sendFile( __dirname + '/public/index.html')
 });
 
 app.listen(port, () => console.log(`Listening to ${port}`))
